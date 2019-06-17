63d6a08e70029084333eeb56569d19c2b53e345c
David-M-Nielsen <monbergnielsen@outlook.com>
2019-06-17 19:20:30 +0200

    added start for doing a virtual maze

--- app/app.ino
+++ app/app.ino
@@ -1,10 +1,31 @@
 #include <usenslib.h>
 #include <movlib.h>
 
+/* XY */
+int lab[5][5] = {
+  {1,1,0,1,1},
+  {1,0,0,1,1},
+  {1,0,1,1,1},
+  {1,0,0,1,1},
+  {1,1,0,1,1}
+};
+
+Usens eyes(7, 8);
+Mov wheels(1, 2, 3, 4, 5, 6);
+
 void setup() {
-	Mov wheels(1,2,3,4,5,6,9600);
-	Usens eyes(7,8, 9600);
+  Serial.begin(9600);
 }
 
 void loop() {
+  while(eyes.isBlocked(10) == false){
+    wheels.advance(160); /* 0 - 255 */
+  }
+  
+  wheels.turnLeft(100);
+  
+  if(eyes.isBlocked(10)){
+    wheels.turnRight(100);
+    wheels.turnRight(100);
+  }
 }
