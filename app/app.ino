d54d803e2195a7a399e2b2896122ad6cef7ef27c
David-M-Nielsen <monbergnielsen@outlook.com>
2019-06-19 17:32:14 +0200

    started testing emu libraries

--- app/app.ino
+++ app/app.ino
@@ -1,11 +1,19 @@
-#include <usenslibemu.h>
 #include <movlibemu.h>
+#include <usenslibemu.h>
+#include <Arduino.h>
 
-Usens eyes(12, 13);
-Mov wheels(4, 5, 6, 7, 8, 9);
+/* 0=open space;1=wall;2=exit */
+char* maze = "1121111011110111101111011";
+int mazeWidth = 5; /* how many characters makes up one row of maze */
 
+Usens eye();
+Mov leg();
+  
 void setup() {
 	Serial.begin(9600);
+	Serial.println(maze);
+	Serial.println(maze[3]);
+	eye.isBlocked(10);
 }
 
 void loop() {
