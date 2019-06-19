c7fe9c903014d93c137c6a29a00bc8fc215f790a
David-M-Nielsen <monbergnielsen@outlook.com>
2019-06-19 19:45:12 +0200

    tried out making the maze a 2d array but passing it back and forth was a pain. Will try using a string again

--- app/app.ino
+++ app/app.ino
@@ -3,28 +3,35 @@
 #include <Arduino.h>
 #include <string.h>
 
-char* maze = "1121111011110111101111011"; /* 0=open space;1=wall;2=exit */
-int mazeWidth = 5; /* how many characters makes up one row of maze */
+/* init sensor emulators */
+Usens centerEye;
+Mov legs;
+
+/* 0=open space;1=wall;2=exit */
+char* maze = "1121111011110111101111011";
+const int width = 5;
+
+/* inital pos of robot */
+int pos = 21;
+
+/* needed values for using pledge algo for maze solving */
+int turnCount = 0;
 
 void setup() {
 	Serial.begin(9600);
-	
-	Usens centerEye;
-	Mov legs;
+
 	printMaze();
 }
 
 /* prints maze in serial monitor */
 void printMaze(){
 	for (int i = 0; i < strlen(maze); i++){
-		Serial.print(maze[i]);
-
-		if(i > 0 && i % mazeWidth == 0){
-			Serial.print("\n");
-		}
+	  if(i > 0 && i % width == 0){
+      Serial.print("\n");
+	  }
+	  Serial.print(maze[i]);
 	}
 }
 
 void loop() {
-
 }
