9440d11740d70ed5d7536a1be89b20c29b3a79be
David-M-Nielsen <monbergnielsen@outlook.com>
2019-06-19 17:47:15 +0200

    added proper print of maze. Imports of library

--- app/app.ino
+++ app/app.ino
@@ -1,19 +1,28 @@
 #include <movlibemu.h>
 #include <usenslibemu.h>
 #include <Arduino.h>
+#include <string.h>
 
-/* 0=open space;1=wall;2=exit */
-char* maze = "1121111011110111101111011";
+char* maze = "1121111011110111101111011"; /* 0=open space;1=wall;2=exit */
 int mazeWidth = 5; /* how many characters makes up one row of maze */
 
-Usens eye();
-Mov leg();
-  
 void setup() {
 	Serial.begin(9600);
-	Serial.println(maze);
-	Serial.println(maze[3]);
-	eye.isBlocked(10);
+	
+	Usens centerEye;
+	Mov legs;
+	printMaze();
+}
+
+/* prints maze in serial monitor */
+void printMaze(){
+	for (int i = 0; i < strlen(maze); i++){
+		Serial.print(maze[i]);
+
+		if(i > 0 && i % mazeWidth == 0){
+			Serial.print("\n");
+		}
+	}
 }
 
 void loop() {
